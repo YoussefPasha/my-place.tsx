ac4f748cbcfa71f586e64090403644e9ebd8a0db
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-23 23:55:44 +0200

    storing image on file system

--- store/places-actions.ts
+++ store/places-actions.ts
@@ -1,7 +1,21 @@
-import { registerRootComponent } from "expo";
-
+import * as FileSystem from "expo-file-system";
 export const ADD_PLACE = "ADD_PLACE";
 
 export const addPlace = (title: string, image: string) => {
-  return { type: ADD_PLACE, placeData: { title, image } };
+  return async (dispatch: any) => {
+    const fileName: string | any = image.split("/").pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath,
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+
+    dispatch({ type: ADD_PLACE, placeData: { title, image: newPath } });
+  };
 };
